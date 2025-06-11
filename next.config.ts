55e81d265f91046ca153d740f44cf015fa7ca0a5
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-06-11 18:46:32 +0530

    image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+        port: "",
+        pathname: "/**",
+      }
     ],
   },
 };
