9c1450872f62edd42791e2b6042264cab1efaa0d
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-06-12 00:15:37 +0530

    social links

--- data/footer.ts
+++ data/footer.ts
@@ -32,3 +32,22 @@
       "Insurance"
     ]
   };
+
+export const socialLinks = [
+  {
+    social:"facebook",
+    image:"https://img.icons8.com/?size=100&id=118497&format=png&color=000000"
+  },
+  {
+    social:"twitter",
+    image:"https://img.icons8.com/?size=100&id=13963&format=png&color=000000" 
+  },
+  {
+    social:"instagram",
+    image:"https://img.icons8.com/?size=100&id=32323&format=png&color=000000"
+  },
+  {
+    social:"linkedin",
+    image:"https://img.icons8.com/?size=100&id=13930&format=png&color=000000"
+  }
+]  
